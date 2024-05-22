03a03bf46144e0c717aff7f73df9e2d3a09ee6b3
Moises <moisesfragahbl@gmail.com>
2024-05-22 13:46:46 -0300

    Att

--- src/main/java/com/example/AtividadeAPI/service/UsuarioService.java
+++ src/main/java/com/example/AtividadeAPI/service/UsuarioService.java
@@ -1,12 +1,13 @@
-package com.example.exemplo1.service;
+package com.example.AtividadeAPI.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.example.exemplo1.model.Usuario;
-import com.example.exemplo1.persistence.UsuarioRepository;
+import com.example.AtividadeAPI.model.Usuario;
+import com.example.AtividadeAPI.persistence.UsuarioRepository;
 
 @Service
 public class UsuarioService {
@@ -19,5 +20,29 @@ public class UsuarioService {
 
     public Usuario inserirUsuario(Usuario usuario) {
         return usuarioRepository.save(usuario);
+    }
+
+    public Usuario atualizarUsuario(Long id, Usuario usuario) {
+        Optional<Usuario> usuarioExistente = usuarioRepository.findById(id);
+        if (usuarioExistente.isPresent()) {
+            Usuario u = usuarioExistente.get();
+            u.setNome(usuario.getNome());
+            u.setLogin(usuario.getLogin());
+            u.setEmail(usuario.getEmail());
+            u.setSenha(usuario.getSenha()); // Considere fazer hash da senha aqui se necessário
+            return usuarioRepository.save(u);
+        } else {
+            return null;
+        }
+    }
+
+    public boolean excluirUsuario(Long id) {
+        Optional<Usuario> usuarioExistente = usuarioRepository.findById(id);
+        if (usuarioExistente.isPresent()) {
+            usuarioRepository.deleteById(id);
+            return true;
+        } else {
+            return false;
+        }
     }
 }
